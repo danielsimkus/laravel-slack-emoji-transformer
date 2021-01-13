f93dccb2b9d197a23095e60f69f27ec5e59ba337
danielsimkus <daniel.simkus@carandclassic.com>
2021-01-13 08:13:17 +0000

    dd

--- src/SlackEmojiTransformerService.php
+++ src/SlackEmojiTransformerService.php
@@ -34,6 +34,7 @@ class SlackEmojiTransformerService
         preg_match_all(':([A-Za-z0-9]):', $message, $emojis);
         $replacements = collect([]);
         $customEmojis = app(LoadCustomEmojis::class)->load($this->token, $this->isBot);
+        dd($customEmojis);
         $defaultEmojis = app(LoadDefaultEmojis::class)->load();
         foreach ($emojis as $emoji) {
             $strippedEmoji = str_replace(':', '', $emoji);
