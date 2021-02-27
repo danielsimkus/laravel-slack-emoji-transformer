b03692849269da41bcd84943a00b256f250d55ee
danielsimkus <daniel.simkus@carandclassic.com>
2021-02-27 15:36:11 +0000

    provider in composer remove bind from boot into register

--- src/Providers/SlackEmojiTransformerProvider.php
+++ src/Providers/SlackEmojiTransformerProvider.php
@@ -7,12 +7,16 @@ use Illuminate\Support\ServiceProvider;
 
 class SlackEmojiTransformerProvider extends ServiceProvider
 {
+
+    public function register()
+    {
+        $this->app->bind(TransformsUrls::class, config('slack-emoji-transformer.url-transformer', DefaultUrlTransformer::class));
+    }
+
     public function boot()
     {
         $this->publishes([
             __DIR__.'/../../configs/config.php' => config_path('slack-emoji-transformer.php'),
         ]);
-
-        $this->app->bind(TransformsUrls::class, config('slack-emoji-transformer.url-transformer', DefaultUrlTransformer::class));
     }
 }
